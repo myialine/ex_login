8c499fa560e3f3ff6ff0ccf22fb7adc12d34f892
Aline Cavalcante <Aline-Cavalcante@live.nmit.ac.nz>
2020-03-30 19:36:40 +1300

    Final - everything works

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+const app = express()
+
+var AccountRoutes = require('./controllers/account_controller');
+var HomeRoutes = require('./controllers/home_controller');
+
+var port = process.env.PORT || 3360;
+
+app.set('view engine','ejs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(session({secret: 'myBananaCakeIsDelicious'}));
+
+app.use('/',AccountRoutes.AccountRoutes);
+
+app.use(function(req,res,next){
+    if(req.session.email == null || req.session.email.length ==0 ){
+        res.redirect('/login'); 
+    }
+    else{
+      next();
+    }
+  });
+  app.use('/',HomeRoutes.HomeRoutes);
+
+app.listen(port);
